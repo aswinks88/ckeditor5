0f9c8693ca76494d609ac88d9d11c86e7bd6dae3
aswinks88 <aswinks88@gmail.com>
2021-05-09 19:24:32 +1200

    added mathtype, codeblock, htmlembed and font plugins

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -31,6 +31,12 @@ import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
 import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
 
+//custom plugin
+import MathType from '@wiris/mathtype-ckeditor5';
+import HtmlEmbed from '@ckeditor/ckeditor5-html-embed';
+import CodeBlock from '@ckeditor/ckeditor5-code-block';
+import Font from '@ckeditor/ckeditor5-font';
+
 export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
@@ -58,7 +64,11 @@ ClassicEditor.builtinPlugins = [
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	TextTransformation
+	TextTransformation,
+	MathType,
+	HtmlEmbed,
+	CodeBlock,
+	Font,
 ];
 
 // Editor configuration.
@@ -81,24 +91,20 @@ ClassicEditor.defaultConfig = {
 			'insertTable',
 			'mediaEmbed',
 			'undo',
-			'redo'
-		]
+			'redo',
+		],
 	},
 	image: {
 		toolbar: [
 			'imageStyle:full',
 			'imageStyle:side',
 			'|',
-			'imageTextAlternative'
-		]
+			'imageTextAlternative',
+		],
 	},
 	table: {
-		contentToolbar: [
-			'tableColumn',
-			'tableRow',
-			'mergeTableCells'
-		]
+		contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells'],
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: 'en'
+	language: 'en',
 };
